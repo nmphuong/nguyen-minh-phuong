e18cdbc75ff2620e179f67a6355e25ed09e34614
bitbucket-cores-vn <bitbucket.cores.vn@gmail.com>
2021-03-26 10:20:46 +0700

    update code

--- send.php
+++ send.php
@@ -1,13 +1,15 @@
 <?php
-if(isset($_POST['message'])){
-    $to      = 'nguyenminhphuong25111999@gmail.com';
-    $subject = $_POST['subject']; 
-    $message = $_POST['message']; 
-    $headers = "From: ".$_POST['name']." <".$_POST['email'].">\r\n"; $headers = "Reply-To: ".$_POST['email']."\r\n"; 
-    $headers = "Content-type: text/html; charset=iso-8859-1\r\n";
-    'X-Mailer: PHP/' . phpversion();
-    if(mail($to, $subject, $message, $headers)) echo json_encode(['success'=>true]); 
-    else echo json_encode(['success'=>false]);
-    exit;
- }
-?>
+
+$searcharray = array();
+parse_str($_POST['data'], $searcharray);
+if($searcharray['send'] == 'SEND'){
+
+$to      = 'nguyenminhphuong25111999@gmail.com';
+$subject = $searcharray['subject']; 
+$message = $searcharray['message']; 
+$headers = "From: ".$searcharray['nam‌​e​']." <".$searcharray['em‌​ail‌​'].">\r\n";
+$headers .= "Reply-To: ".$searcharray['em‌​ail‌​']."\r\n"; 
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+$headers .= 'X-Mailer: PHP/' . phpversion();
+mail($to, $subject, $message, $headers);
+}
